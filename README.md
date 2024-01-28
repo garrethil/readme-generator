52eb35285a4f720297f404f2d25753aa4f1b60a5
garrethil <garrethildebrandt@gmail.com>
2024-01-28 13:54:51 -0500

    add badge to top of created read me file that matches the users input

---
+++ README.md
@@ -0,0 +1,39 @@
+# Readme Generator
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  It uses inquirer to collect the information about a project in the command line to then produce a proffessional readme.md from the collected inputs.
+
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  
+  ## Installation
+  you must install inquirer to run the readme generator.
+  
+  ## Usage
+  when finishing up a large project it can be laborious to then sit down and make a full readme file in markdown format. The readme generator is meant to allow the user to just answer a short list of questions and then produce a full complete readme file to go along with your project.  
+
+  
+  ## License
+  MIT License
+
+
+  ## How to Contribute
+  N/A
+    
+  ## Tests
+  N/A
+
+  ## Questions
+  [Github Profile](https://github.com/garrethil)
+
+  For any further questions you may use this email: garrethil@hotmail.com
+  
--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+const apacheBadge =
+  "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+const mitBadge =
+  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+let badge;
 
-const generateREADME = ({ title, description, installation, usage, license, contribute}) =>
+const generateReadme = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contribute,
+  tests,
+  github,
+  email,
+}) =>
   `# ${title}
+
+  ${badge}
 
   ## Description
   ${description}
 
 
   ## Table of Contents
-  - - [Installation](#installation)
+  - [Installation](#installation)
   - [Usage](#usage)
   - [License](#license)
   - [Contributing](#contributing)
@@ -26,59 +43,86 @@ const generateREADME = ({ title, description, installation, usage, license, cont
 
   
   ## License
-    ${license}
+  ${license}
 
 
   ## How to Contribute
   ${contribute}
-
-
-
+    
   ## Tests
-
+  ${tests}
 
   ## Questions
-  
+  [Github Profile](https://github.com/${github})
+
+  For any further questions you may contact me at this email: ${email}
   `;
 
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'title',
-      message: 'What is your title of your project?',
+      type: "input",
+      name: "title",
+      message: "What is your title of your project?",
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'Please explaining the what, why, and how of your project.',
+      type: "input",
+      name: "description",
+      message: "Please explaining the what, why, and how of your project.",
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'What installation steps are there to run your project?',
+      type: "input",
+      name: "installation",
+      message: "What installation steps are there to run your project?",
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'Explain how you expect someone to use your product.',
+      type: "input",
+      name: "usage",
+      message: "Explain how you expect someone to use your product.",
     },
     {
-      type: 'list',
-      name: 'license',
-      message: 'Please select a license for your project.',
-      choices: ['MIT License','Apache License 2.0', 'Other'],
+      type: "list",
+      name: "license",
+      message: "Please select a license for your project.",
+      choices: ["MIT License", "Apache License 2.0", "Other"],
     },
     {
-      type: 'input',
-      name: 'contribute',
-      message: 'How should others go about making their own contributions to your project?',
+      type: "input",
+      name: "contribute",
+      message:
+        "How should others go about making their own contributions to your project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "Are there any tests you wrote for this project? How could we run them?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter your github username.",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter an email users/contributers could reach you.",
     },
   ])
   .then((answers) => {
-    const readmeContent = generateREADME(answers);
+    if (answers.license === "MIT License") {
+      badge = mitBadge;
+    } else if (answers.license === "Apache License 2.0") {
+      badge = apacheBadge;
+    } else {
+      badge = "No license for badge";
+    }
 
-    fs.writeFile('README.md', readmeContent, (err) =>
-      err ? console.log(err) : console.log('Your README.md file was created for your project!')
+    const readmeContent = generateReadme(answers);
+
+    fs.writeFile("README.md", readmeContent, (err) =>
+      err
+        ? console.log(err)
+        : console.log("Your README.md file was created for your project!")
     );
   });
